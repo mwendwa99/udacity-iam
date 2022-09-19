9589a0e8c475db450856a62b0d7db6077bbc5656
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-09-19 08:25:30 +0300

    update codebase

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -13,14 +13,11 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-
-
 def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-
 
 '''
 db_drop_and_create_all()
@@ -28,27 +25,14 @@ db_drop_and_create_all()
     can be used to initialize a clean database
     !!NOTE you can change the database_filename variable to have multiple verisons of a database
 '''
-
-
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
-    # add one demo row which is helping in POSTMAN test
-    drink = Drink(
-        title='water',
-        recipe='[{"name": "water", "color": "blue", "parts": 1}]'
-    )
-
-
-    drink.insert()
-# ROUTES
 
 '''
 Drink
 a persistent drink entity, extends the base SQLAlchemy Model
 '''
-
-
 class Drink(db.Model):
     # Autoincrementing, unique primary key
     id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
@@ -56,13 +40,12 @@ class Drink(db.Model):
     title = Column(String(80), unique=True)
     # the ingredients blob - this stores a lazy json blob
     # the required datatype is [{'color': string, 'name':string, 'parts':number}]
-    recipe = Column(String(180), nullable=False)
+    recipe =  Column(String(180), nullable=False)
 
     '''
     short()
         short form representation of the Drink model
     '''
-
     def short(self):
         print(json.loads(self.recipe))
         short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
@@ -76,7 +59,6 @@ class Drink(db.Model):
     long()
         long form representation of the Drink model
     '''
-
     def long(self):
         return {
             'id': self.id,
@@ -93,7 +75,6 @@ class Drink(db.Model):
             drink = Drink(title=req_title, recipe=req_recipe)
             drink.insert()
     '''
-
     def insert(self):
         db.session.add(self)
         db.session.commit()
@@ -106,7 +87,6 @@ class Drink(db.Model):
             drink = Drink(title=req_title, recipe=req_recipe)
             drink.delete()
     '''
-
     def delete(self):
         db.session.delete(self)
         db.session.commit()
@@ -120,7 +100,6 @@ class Drink(db.Model):
             drink.title = 'Black Coffee'
             drink.update()
     '''
-
     def update(self):
         db.session.commit()
 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-p2nopehd.eu.auth0.com', // the auth0 domain prefix
-    audience: 'cshop-api', // the audience set for the auth0 app
-    clientId: 'L1kX5iIBSpHymRAdMqhCSHz95uEhijEB', // the client id generated for the auth0 app
+    url: 'tomariken', // the auth0 domain prefix
+    audience: 'coffee', // the audience set for the auth0 app
+    clientId: 'ypVeYkA1TRzchPNpE6j2Lvh76COwOHIT', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
